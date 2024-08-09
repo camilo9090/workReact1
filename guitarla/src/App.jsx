4ba9bfc28d16422b6a651b6f0ba5abe2762176ea
camilo9090 <camilou9090@gmail.com>
2024-08-09 16:40:50 -0500

    se crearon los UseState y se itero sobre los productos

--- guitarla/src/App.jsx
+++ guitarla/src/App.jsx
@@ -1,7 +1,11 @@
 import Header from "./components/Header";
 import Guitar from "./components/Guitar";
+import { useState } from "react";
+import { db } from "./data/db";
 
 function App() {
+  const [data, setdata] = useState(db);
+
   return (
     <>
       <Header />
@@ -10,15 +14,11 @@ function App() {
         <h2 className="text-center">Nuestra Colección</h2>
 
         <div className="row mt-5">
-
-            <Guitar/>
-            <Guitar/>
-            <Guitar/>
-            <Guitar/>
-            <Guitar/>
-            <Guitar/>
-            <Guitar/>
-            <Guitar/>
+          {data.map((guitar) => (
+            <Guitar 
+            key={guitar.id} 
+            guitar={guitar} />
+          ))}
         </div>
       </main>
 
